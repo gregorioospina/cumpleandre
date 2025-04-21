b456265e06c2dfecf79817ebc6da907135e92798
gregorio_ospina <gregorio.ospina@rappi.com>
2025-04-21 18:58:13 -0500

    va

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
 	return (
 		<div
 			className={`grid ${
-				showCentralDialog ? "bg-white text-black" : "bg-black text-white"
+				showCentralDialog ? " text-black" : "bg-black text-white"
 			} grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
 			{showCentralDialog && (
 				<>
